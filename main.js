7a0ebec82f65e204a04f4c88d93a23f9b0e3ea10
Stephen Urena <surena@Stephens-MacBook-Air.local>
2017-04-04 16:39:37 -0700

    added blank functions to user application

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
+//load initial packages to main.js
 var BasicCard = require("./basic.js");
 var ClozeCard = require("./cloze.js");
 var inquirer = require("inquirer");
 var fs = require("fs");
 
-var startString = [
+//
+var startOptions = [
   {
     type: "list",
     name: "userSelection",
-    message: "",
-    choices: ["View", "Create"]
+    message: "Would you like to study or create a new flashcard?",
+    choices: ["Study", "Create"]
   }
 ];
 
-var flashCardOpt = [
+var flashCardOption = [
   {
     type: "list",
     message: "What type of flash card do you want?",
@@ -20,8 +22,27 @@ var flashCardOpt = [
   }
 ]
 
+//Initial user prompt to initilize app, to either study cards or create new ones
 function startUp(){
   console.log("startUp function loaded");
+  inquirer.prompt(startOptions).then(function(response) {
+    switch (response.userSelection) {
+      case ("Study"):
+        studyCards();
+        break;
+      case ("Create"):
+        createCards();
+        break;
+      // default: //don't think i need a default because it is prompting user with a list
+    }
+  });
 }
 
+function studyCards(){
+  console.log("studyCards is loaded");
+}
+
+function createCards(){
+  console.log("CreateCards is loaded");
+}
 startUp()
